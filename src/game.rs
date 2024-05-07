b9ee0cb6d9228f568c2493e8caa8cff0cc7acbf9
estn <leolaijiang@gmail.com>
2024-05-08 00:18:30 +0200

    ball collisions

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,15 @@
-use ggez::{mint::{Point2, Vector2}, *};
+use ggez::{conf::WindowMode, mint::{Point2, Vector2}, *};
 
-const SCREEN_WIDTH: f32 = 400.0;
-const SCREEN_HEIGHT: f32 = 400.0;
-const BALL_VELOCITY: f32 = 200.0;
+const SCREEN_WIDTH: f32 = 800.0;
+const SCREEN_HEIGHT: f32 = 600.0;
+const SCREEN_WIDTH_MID: f32 = SCREEN_WIDTH / 2.0;
+const SCREEN_HEIGHT_MID: f32 = SCREEN_HEIGHT / 2.0;
+
+const BALL_VELOCITY: f32 = 500.0;
+const BALL_RADIUS: f32 = 10.0;
+const BALL_RADIUS_MID: f32 = BALL_RADIUS / 2.0;
 
 struct State {
-    // delta_time: std::time::Duration,
     ball_pos: mint::Point2<f32>,
     ball_vel: mint::Vector2<f32>,
 }
@@ -13,8 +17,7 @@ struct State {
 impl State {
     pub fn new(ctx: &mut Context) -> Self {
         State {
-            // delta_time: std::time::Duration::new(0, 0),
-            ball_pos: mint::Point2{x: 0.0, y: 0.0} ,
+            ball_pos: mint::Point2{x: SCREEN_WIDTH_MID - BALL_RADIUS_MID, y: SCREEN_HEIGHT_MID - BALL_RADIUS_MID} ,
             ball_vel: mint::Vector2{x: BALL_VELOCITY, y: BALL_VELOCITY},
         }
     }
@@ -23,10 +26,21 @@ impl State {
 impl ggez::event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         let delta_time = ctx.time.delta().as_secs_f32();
+
+        // ball movement
         self.ball_pos = mint::Point2 {
             x: self.ball_pos.x + self.ball_vel.x * delta_time,
-            y: self.ball_pos.x + self.ball_vel.y * delta_time
+            y: self.ball_pos.y + self.ball_vel.y * delta_time
         };
+
+        // ball collisions
+        if (self.ball_pos.x > SCREEN_WIDTH || self.ball_pos.x < 0.0) {
+            self.ball_vel.x *= -1.0;
+        }
+        if (self.ball_pos.y > SCREEN_HEIGHT || self.ball_pos.y < 0.0) {
+            self.ball_vel.y *= -1.0;
+        }
+
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
@@ -38,7 +52,7 @@ impl ggez::event::EventHandler for State {
             ctx, // sagt ggez wohin er malen soll - hier also auf unseren context
             graphics::DrawMode::fill(), // mal modus für unser ball
             self.ball_pos,
-            19.0, // radius 
+            BALL_RADIUS,
             0.1, // tolerance (idk) 
             graphics::Color::WHITE,
         )?;
@@ -47,6 +61,7 @@ impl ggez::event::EventHandler for State {
         let mut draw_parameters = graphics::DrawParam::default();
 
         draw_parameters.dest(self.ball_pos);
+
         canvas.draw(&ball, draw_parameters);
         canvas.finish(ctx);
         Ok(())
@@ -54,12 +69,9 @@ impl ggez::event::EventHandler for State {
 }
 
 pub fn main() {
-   // let state = State {
-   //     delta_time: std::time::Duration::new(0, 0),
-   //     ball_pos: na::Point2::new(screen_width, screen_height),
-   //     ball_vel: na::Vector2::new(screen_width, screen_height),
-   // };
-    let context_builder = ggez::ContextBuilder::new("pingpangpong", "Lai");
+
+    let context_builder = ggez::ContextBuilder::new("pingpangpong", "Lai")
+        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_WIDTH, SCREEN_HEIGHT).borderless(true));
     let (mut context, event_loop) = context_builder.build().unwrap();
     let mut state = State::new(&mut context);
     context.gfx.set_window_title("pingpangpong");
