8ea0ccd7a9fb20d1051a9b5328f4cbd0c76ba7d5
estn <leolaijiang@gmail.com>
2024-05-08 01:15:55 +0200

    ball mvment & code restructure using structs

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use ggez::{conf::WindowMode, mint::{Point2, Vector2}, *};
+use ggez::graphics::Rect;
 
 const SCREEN_WIDTH: f32 = 800.0;
 const SCREEN_HEIGHT: f32 = 600.0;
@@ -9,16 +10,56 @@ const BALL_VELOCITY: f32 = 500.0;
 const BALL_RADIUS: f32 = 10.0;
 const BALL_RADIUS_MID: f32 = BALL_RADIUS / 2.0;
 
+const PAD_VELOCITY: f32 = 500.0;
+const PAD_LENGTH: f32 = 100.0;
+const PAD_WIDTH: f32 = 10.0;
+const PAD_LEFT_EDGE: f32 = 40.0;
+
+struct Pad {
+    rect: Rect,
+    pos: mint::Point2<f32>,
+    vel: mint::Vector2<f32>,
+}
+
+impl Pad {
+    pub fn new() -> Self {
+        Pad {
+            rect: ggez::graphics::Rect::new(
+                PAD_LEFT_EDGE,
+                SCREEN_HEIGHT_MID - (PAD_LENGTH / 2.0),
+                PAD_WIDTH,
+                PAD_LENGTH,
+            ),
+            pos: mint::Point2{x: PAD_LEFT_EDGE, y: SCREEN_HEIGHT_MID},
+            vel: mint::Vector2{x: 0.0, y: PAD_VELOCITY},
+        }
+    }
+}
+
+struct Ball {
+    pos: mint::Point2<f32>,
+    vel: mint::Vector2<f32>,
+}
+
+impl Ball {
+    pub fn new() -> Self {
+        Ball {
+            pos: mint::Point2{x: SCREEN_WIDTH_MID - BALL_RADIUS_MID, y: SCREEN_HEIGHT_MID - BALL_RADIUS_MID},
+            vel: mint::Vector2{x: BALL_VELOCITY, y: BALL_VELOCITY},
+        }
+    }
+}
+
 struct State {
-    ball_pos: mint::Point2<f32>,
-    ball_vel: mint::Vector2<f32>,
+    ball: Ball,
+    pad: Pad,
 }
 
 impl State {
     pub fn new(ctx: &mut Context) -> Self {
         State {
-            ball_pos: mint::Point2{x: SCREEN_WIDTH_MID - BALL_RADIUS_MID, y: SCREEN_HEIGHT_MID - BALL_RADIUS_MID} ,
-            ball_vel: mint::Vector2{x: BALL_VELOCITY, y: BALL_VELOCITY},
+            ball: Ball::new(),
+            pad: Pad::new(),
         }
     }
 }
@@ -28,18 +69,22 @@ impl ggez::event::EventHandler for State {
         let delta_time = ctx.time.delta().as_secs_f32();
 
         // ball movement
-        self.ball_pos = mint::Point2 {
-            x: self.ball_pos.x + self.ball_vel.x * delta_time,
-            y: self.ball_pos.y + self.ball_vel.y * delta_time
+        self.ball.pos = mint::Point2 {
+            x: self.ball.pos.x + self.ball.vel.x * delta_time,
+            y: self.ball.pos.y + self.ball.vel.y * delta_time
         };
 
         // ball collisions
-        if (self.ball_pos.x > SCREEN_WIDTH || self.ball_pos.x < 0.0) {
-            self.ball_vel.x *= -1.0;
+        if (self.ball.pos.x > SCREEN_WIDTH || self.ball.pos.x < 0.0) {
+            self.ball.vel.x *= -1.0;
         }
-        if (self.ball_pos.y > SCREEN_HEIGHT || self.ball_pos.y < 0.0) {
-            self.ball_vel.y *= -1.0;
+        if (self.ball.pos.y > SCREEN_HEIGHT || self.ball.pos.y < 0.0) {
+            self.ball.vel.y *= -1.0;
         }
+
+        // TODO: pad_movement with key input
+        // TODO: pad-ball collisions
+        // TODO: Score & reset
 
         Ok(())
     }
@@ -51,18 +96,30 @@ impl ggez::event::EventHandler for State {
         let ball = graphics::Mesh::new_circle(
             ctx, // sagt ggez wohin er malen soll - hier also auf unseren context
             graphics::DrawMode::fill(), // mal modus für unser ball
-            self.ball_pos,
+            self.ball.pos,
             BALL_RADIUS,
             0.1, // tolerance (idk) 
             graphics::Color::WHITE,
         )?;
 
-        // now draw the ball onto canvas
+        // make the pad mesh
+        let pad = graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::stroke(1.0),
+                self.pad.rect,
+                graphics::Color::WHITE,
+            )?;
+
+        // TODO: Score System
+
+        // set the params for drawing (this gets the position done)
         let mut draw_parameters = graphics::DrawParam::default();
 
-        draw_parameters.dest(self.ball_pos);
+        draw_parameters.dest(self.ball.pos);
 
+        // I like it, picasso
         canvas.draw(&ball, draw_parameters);
+        canvas.draw(&pad, draw_parameters);
         canvas.finish(ctx);
         Ok(())
     }
